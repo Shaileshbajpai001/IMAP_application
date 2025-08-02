0d0a04dc78f389b4a22908e0c447706a97ebd2c9
Shailesh Bajpai <shaileshshaileshbajpai4488@gmail.com>
2025-08-02 18:14:04 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const serverless = require("serverless-http");
+const serverless = require("serverless-http"); // for vercel
 const { startIMAPSync } = require("./imap/imapClient");
 const Email = require("./models/emailModel");
 
